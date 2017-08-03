1b652f987f87a2b276ea6da24dc0797d33884b28
wangtao <wangtao@weibopay.com>
2017-08-03 15:00:08 +0800

    fix copy files repo name

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -56,7 +56,7 @@ function createPackageFile() {
     } = packageData;
 
     const minimalPackage = {
-      name: 'material-ui',
+      name: 'react-immutable-treeview',
       author,
       version,
       description,
